78b98241aec7fbbfd38e148f1e44243de1c629c7
tufedtm <tufedtm@gmail.com>
2017-11-05 22:40:48 +0300

    fin

--- oboi_shop/settings.py
+++ oboi_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import socket
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o27531usngo9i3(d$-1$-7v8qel26!bxspz57y*&k@xb2h)ab_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if socket.gethostname() in ('ttm-mint-home',):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
